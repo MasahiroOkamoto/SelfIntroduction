2b2732dcaf79855d655c357e031bef81bd135b75
okamotchan <m.okamotchan@gmail.com>
2019-05-22 13:02:50 +0900

    moromoro

--- _site/static/script.js
+++ _site/static/script.js
@@ -4,11 +4,6 @@ $(function(){
       $(this).children('span').animate({
         'font-size':'30px'
       }, 300);
-    },
-    function(){
-      $(this).children('span').animate({
-        'font-size':'30px'
-      }, 300);
     });
 
   $('#top-btn').click(function(){
